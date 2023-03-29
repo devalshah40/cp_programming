fbd8dccee7eb4b5c0fce6a87b00c368c608b8cc6
Deval Shah <devalshah40@gmail.com>
2023-03-29 14:48:41 +0530

    added

--- demo/Advanced DSA  Linked List - 1/linked_list.js
+++ demo/Advanced DSA  Linked List - 1/linked_list.js
@@ -1,94 +1,67 @@
-// YOUR CODE GOES HERE
 // Please take input and print output to standard input/output (stdin/stdout)
 // DO NOT USE ARGUMENTS FOR INPUTS
-
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
-}
+let root = new Node();
+let size_of_ll = 0;
 class LinkedList {
-  head = null;
-  constructor() {}
-
-  insert_node(pos, no) {
-    let newNode = new Node(no);
-    // if position is 1
-    if (pos <= 1) {
-      newNode.next = this.head;
-      this.head = newNode;
-      return newNode;
-    }
-
-    // head is null
-    if (this.head === null) {
-      this.head = newNode;
-      return newNode;
-    }
-
-    let temp = this.head;
-    let len = 1;
-    while (len < no - 1 && temp.next) {
-      temp = temp.next;
-      len++;
-    }
-
-    newNode.next = temp.next;
-    temp.next = newNode;
-
-    return this.head;
+  constructor() {
+    root = null;
+    size_of_ll = 0;
   }
 
-  delete_node(pos) {
-    // head is null
-    if (this.head === null) {
-      return this.head;
+  insert_node(position, value) {
+    let temp = new Node(0);
+    temp.data = value;
+    // insert at head
+    if (position == 1) {
+      temp.next = root;
+      root = temp;
+    } else {
+      let count = 1;
+      let prev = root;
+      // traverse till the previous node
+      while (count < position - 1) {
+        prev = prev.next;
+        count++;
+      }
+      temp.next = prev.next;
+      prev.next = temp;
     }
+    size_of_ll++;
+  }
 
-    // if position is 0 or -1
-    if (pos <= 1) {
-      const temp = this.head;
-      this.head = this.head.next;
-      temp.next = null;
-      return this.head;
+  delete_node(position) {
+    if (position >= 1 && position <= size_of_ll) {
+      let temp = null;
+      // delete at head
+      if (position == 1) {
+        temp = root;
+        root = root.next;
+      } else {
+        let count = 1;
+        let prev = root;
+        // traverse till the previous node
+        while (count < position - 1) {
+          prev = prev.next;
+          count++;
+        }
+        temp = prev.next;
+        prev.next = prev.next.next;
+      }
+      size_of_ll--;
     }
-
-    let temp = this.head;
-    let len = 1;
-    while (len < pos - 2 && temp.next) {
-      len++;
-      temp = temp.next;
-    }
-    if (temp.next) temp.next = temp.next.next;
-    return this.head;
   }
 
   print_ll() {
-    let ans = '';
-    let temp = this.head;
-
-    while (temp !== null) {
-      ans += temp.data + ' ';
+    let temp = root;
+    let flag = 0;
+    // traverse the entire linked list
+    while (temp != null) {
+      if (flag == 0) {
+        process.stdout.write(temp.data);
+        flag = 1;
+      } else process.stdout.write(' ' + temp.data);
       temp = temp.next;
     }
-    console.log(ans.slice(0, -1));
-    return;
+    process.stdout.write('\n');
   }
 }
-let head = new Node(10);
-let second = new Node(20);
-head.next = second;
-let third = (second.next = new Node(30));
-second.next = third;
-
-console.log(head);
-// console.log(second);
-// console.log(third);
-
-printAll(head);
-// 10 -> 20 -> 30
-//  0 ->  1 ->  2
-// Insert at 3rd location
-head = insertLinkedList(head, 40, 3);
-printAll(head);
